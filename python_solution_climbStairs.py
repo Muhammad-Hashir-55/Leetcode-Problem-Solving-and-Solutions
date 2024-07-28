a60b4b3b7363c53bc20a847eac44ef71da62680c
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-07-28 20:07:11 +0500

    Problem 70 python solution
    this is problem 70 solution in python 
    but the runtime efficiency is lower than C++ solution

---
+++ python_solution_climbStairs.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if (n<= 1):
+            return 1
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i -1] + dp[i -2]
+        return dp[n]
+
+
+check  = Solution()
+print(check.climbStairs(10))
+        
