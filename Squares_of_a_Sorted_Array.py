dc477177bab0dd08363a1cda19c9ad379c2adb7e
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-09-20 18:17:50 +0500

    Problem 977 Easy Solution in Python

---
+++ Squares_of_a_Sorted_Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            nums[i] = nums[i]**2
+        nums.sort()
+        return nums
+        
