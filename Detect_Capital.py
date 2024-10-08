136356c09d4407db8daf2bd4adcb64e79c57637d
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-10-08 21:45:28 +0500

    Problem 520 Easy Solution in Python & Few Lines, Good Speed

---
+++ Detect_Capital.py
@@ -0,0 +1,12 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if(word == word.upper()):
+            return True
+        elif(word == word.capitalize()):
+            return True
+        elif(word == word.lower()):
+            return True
+        else:
+            return False
+        
+        
