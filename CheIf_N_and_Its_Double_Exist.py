5db10db7cc1b15ab13b90bf938b8e83ee8a04223
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-10-16 12:55:06 +0500

    Problem 1346 Best Solution in Python

---
+++ CheIf_N_and_Its_Double_Exist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                if(arr[i]==2*arr[j] or arr[j]==2*arr[i]):
+                    return True
+        return False
+
+        
