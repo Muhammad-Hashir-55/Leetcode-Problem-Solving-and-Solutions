4b28e39d44e8b194708a61f0084cb4a809d1ff1e
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-10-31 19:28:01 +0500

    Poblem 1869 Easy Solution in Python

---
+++ Longer_Contiguous_Segments_of_Ones_than_Zeros.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
+        ones = []
+        zeros = []
+        o = 0
+        z = 0
+        for i in s:
+            if(i =='0'):
+                z +=1
+                ones.append(o)
+                o = 0
+            else:
+                o +=1
+                zeros.append(z)
+                z = 0
+        ones.append(o)
+        zeros.append(z)
+        if(max(ones)<= max(zeros)):
+            return False
+        else:
+            return True
+
+        
