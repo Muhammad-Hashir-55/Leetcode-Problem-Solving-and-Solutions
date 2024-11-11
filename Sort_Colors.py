c35f15fd7df6e307b41f0e23cf8dfa4a9d76b22d
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-11-11 19:01:37 +0500

    Problem 75 Beginner Level Solution in Python

---
+++ Sort_Colors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if(nums[i]>nums[j]):
+                    nums[i],nums[j] = nums[j],nums[i]
+        
+        
