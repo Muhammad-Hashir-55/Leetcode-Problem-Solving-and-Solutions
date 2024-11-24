46c280e020364bb06cf61a214f1e0c644461b6e5
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2024-11-24 12:20:16 +0500

    Problem 938 Simple and Fast Solution in C++

---
+++ Range_Sum_of_BST.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+vector<long>ans;
+void inorder(TreeNode * root,int low,int high){
+    if(root == NULL){
+        return ;
+    }
+    inorder(root->left,low,high);
+    if(root->val>= low && root->val <=high){
+        ans.push_back(root->val);
+    }
+    inorder(root->right,low,high);
+}
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        inorder(root,low,high);
+        long n = ans.size();
+        long sum = 0;
+        for(int i = 0;i<n;i++){
+            sum += ans[i];
+        }
+        return sum;
+        
+
+    }
+};
