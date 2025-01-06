3c97062d135400491955eea8c8f868d10c7d3a36
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-01-06 14:32:17 +0500

    Problem 81 Simple Solution in Python

---
+++ Search_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if (target not in nums):
+            return False
+        else:
+            return True
+        
