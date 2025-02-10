6855a38b52db87ec7fea3bcbb183ad2af58c023e
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-02-11 00:26:25 +0500

    Problem 155 Simple Solution but get_min function is O(n) this needs to be improved

---
+++ Min_Stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.s =[]
+    
+        
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        
+        
+
+    def pop(self) -> None:
+        x= self.s.pop()
+        
+
+    def top(self) -> int:
+        return self.s[-1]
+        
+
+    def getMin(self) -> int:
+        return min(self.s)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
