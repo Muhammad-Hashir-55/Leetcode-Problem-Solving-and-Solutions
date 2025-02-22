a397681821f460a857e87f8ddc8dfac4ed875385
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-02-23 00:34:55 +0500

    Problem 1985 Simple Solution in Python

---
+++ Find_the_Kth_Largest_Integer_in_the_Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        arr=  []
+        for i in nums:
+            arr.append(int(i))
+
+        arr.sort()
+        return str(arr[-k])
+        
