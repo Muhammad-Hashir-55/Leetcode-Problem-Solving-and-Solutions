41a84d418bfc2ead3500695c6c35e6cac70e0884
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-08-17 11:54:56 +0500

    Problem 1693 Simple Solution in PostgreSQL

---
+++ Daily_Leads_and_Partners.sql
@@ -0,0 +1,3 @@
+-- Write your PostgreSQL query statement below
+select date_id,make_name, count( distinct lead_id) as unique_leads, count(distinct partner_id) as unique_partners from dailysales
+group by date_id, make_name
