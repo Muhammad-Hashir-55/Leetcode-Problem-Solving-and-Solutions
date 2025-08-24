b5e866bc33b2f09911d0b096626fa5bbf4b5bcff
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-08-24 11:43:35 +0500

    Problem 2666 Simple Solution in JS for Beginners by using a Boolean check

---
+++ Allow_One_Function_Call.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let c = false;
+    
+    return function(...args){
+        if(c === false){
+            res = fn(...args);
+            c = true;
+            return res
+        }
+        else {
+            return undefined
+        }
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
