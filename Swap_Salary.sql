a35a3fa9101e59bc636c16eefb10a39f277c1ccd
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-09-16 14:28:49 +0500

    Problem 627 Solution with only one Update Statement

---
+++ Swap_Salary.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+UPDATE salary
+SET sex = CASE
+             WHEN sex = 'm' THEN 'f'
+             WHEN sex = 'f' THEN 'm'
+             ELSE sex
+          END;
