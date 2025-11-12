3ba5db11292bf2722048d456bda60b08e0f7fc45
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-11-12 13:59:53 +0500

    Problem 2654 Best Solution in Python for Beginners

---
+++ Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = inf
+        ones = nums.count(1)
+        if(ones):
+            return n-ones
+        for i in range(n):
+            g = nums[i]
+            for j in range(i+1,n):
+                g = gcd(g,nums[j])
+                if(g == 1):
+                    res = min(res,j-i)
+        if(res == inf):
+            return -1
+        return res + n-1
+        
+
+        
